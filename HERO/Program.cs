d0f27a32f2526c80c4848ec8b33f9e987974eae2
HAELGEE <helge1337@live.com>
2025-08-26 08:55:10 +0200

    Updated so Admin account creates when program starts with admin Rights ofc

--- HERO/Program.cs
+++ HERO/Program.cs
@@ -14,6 +14,31 @@ namespace HERO
         {
             using (var db = new MyDbContext())
             {
+                // Lägga till Admin om det inte finns någon admin
+                List<User> users = db.User.ToList();
+                bool foundAdmin = false;
+                foreach (var user in users)
+                {
+                    if(user.Firstname == "Admin")
+                        foundAdmin = true;
+                }
+
+                if(!foundAdmin)
+                {
+                    string hashedPassword = BC.EnhancedHashPassword("123Admin321", 14);
+
+                    db.User.Add(new User
+                    {
+                        Firstname = "Admin",
+                        Lastname = "Admin",
+                        Email = "admin@admin.admin",
+                        Password = hashedPassword,
+                        IsAdmin = true
+                    });
+                    db.SaveChanges();                    
+                }
+
+
                 await Menu.Menu.StartMenu();
                 //await Attack.Attacking();
                 //Level.LevelUpStats();
